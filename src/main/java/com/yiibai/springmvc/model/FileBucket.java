9b4ae8036ce2876a4a34ee3d75030fd6f8faa7a5
qqpengqq <qwer492915298@126.com>
2018-07-27 13:52:37 +0800

    first commit

---
+++ src/main/java/com/yiibai/springmvc/model/FileBucket.java
@@ -0,0 +1,16 @@
+package com.yiibai.springmvc.model;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileBucket {
+
+	MultipartFile file;
+	
+	public MultipartFile getFile() {
+		return file;
+	}
+
+	public void setFile(MultipartFile file) {
+		this.file = file;
+	}
+}
